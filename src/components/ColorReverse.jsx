d1370c0237875ff5843199f2264feee9b208752f
ramirez <prlara2000@gmail.com>
2022-05-15 15:35:42 -0500

    style reverse button

--- src/components/ColorReverse.jsx
+++ src/components/ColorReverse.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 function ColorReverse({ onReverse }) {
-  return <button onClick={onReverse}>Change Color</button>;
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <button
+        onClick={onReverse}
+        style={{
+          padding: "0.3rem",
+          cursor: "pointer",
+          color: "white",
+          backgroundColor: "black",
+          border: "1px white solid",
+          borderRadius: "5px",
+          width: "50%",
+          fontFamily: "cursive",
+          fontWeight: "900",
+          letterSpacing: "0.5rem",
+        }}
+      >
+        CHANGE COLOR
+      </button>
+    </div>
+  );
 }
 
 export default ColorReverse;
--- src/components/FeedbackStat.jsx
+++ src/components/FeedbackStat.jsx
@@ -5,10 +5,10 @@ function FeedbackStat({ feedback }) {
     feedback.reduce((acc, cur) => {
       return acc + cur.rating;
     }, 0) / feedback.length;
-  console.log(average);
+
   average = average.toFixed(1).replace(/[.,]0$/, "");
   return (
-    <div className="feedback-stats">
+    <div className="feedback-stats" style={{ margin: "1rem 0 0.5rem 0" }}>
       <h4>{feedback.length} Reviews</h4>
       <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
     </div>
